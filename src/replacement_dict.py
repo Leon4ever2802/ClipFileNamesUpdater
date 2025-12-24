ce3611430e20a8334740f0eed3339a76ade7ce80
leonreusch <leon@reusch-home.de>
2025-12-24 14:15:24 +0100

    feat: add removing of geforce branding

--- src/replacement_dict.py
+++ src/replacement_dict.py
@@ -1,4 +1,11 @@
 known_games = {
+    "Ark Survival Evolved": "ARK",
     "Counter-strike  Global Offensive": "CSGO",
-    "Counter-strike 2": "CS2"
+    "Counter-strike 2": "CS2",
+    "Escape from Tarkov": "EFT",
+    "Escape from Tarkov Arena": "EFT Arena",
+    "League of Legends": "LoL",
+    "Mount & Blade II Bannerlord": "Bannerlord",
+    "Tom Clancy's Ghost Recon Wildlands": "Wildlands",
+    "Tom Clancy's Rainbow Six Siege": "R6",
 }
